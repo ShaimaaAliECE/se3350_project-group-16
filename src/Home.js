0e97dea419325512d5a79e518328973f189d29d7
Jasdeep Singh <jsing287@uwo.ca>
2022-02-03 22:27:30 -0500

    added

--- src/Home.js
+++ src/Home.js
@@ -7,12 +7,33 @@ import Level1 from './Level1';
 
 const Home = ()=>
 {
+
+    const [levelOne, setOne] = useState(true);
+    const [levelTwo, setTwo] = useState(false);
+    const [levelThree, setThree] = useState(false);
+
+
+
+    function nextLevel()
+    {
+
+    }
+
+
+
+
+
     return(
 
         <div className="sorting-tutor">
            <div className="title">Welcome To The Sorting Tutor</div>
            <div name="body">
                      <Level1/>
+                     <div>
+                         {}
+                         {}
+                         {}
+                     </div>
            </div>
            
         </div>
