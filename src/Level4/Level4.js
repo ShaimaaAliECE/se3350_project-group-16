e8c25b8d15a5337cfbd32aeb70481c3835ad141f
David Esposto <desposto@uwo.ca>
2022-03-11 17:16:20 -0500

    David's Commits

--- src/Level4/Level4.js
+++ src/Level4/Level4.js
@@ -1,99 +1,99 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import {mergeSortingAlgo} from  '../MergeSort';
-import './Level4.css';
-import  Steps4 from './Steps4';
+import { mergeSortingAlgo } from "../MergeSort";
+import "./Level4.css";
+import Steps4 from "./Steps4";
 
-const Level4 = (props)=>{
+const Level4 = (props) => {
+  const [sort, setSort] = useState([]);
+  const [unsort, setUnSort] = useState([]);
+  const [branch, setBranch] = useState([]);
+  const [generate, setGenerate] = useState(false);
+  const [toggleStep, setStep] = useState(true);
 
+  function startSort() {
+    let nums = [];
 
-    const[sort, setSort]=useState([]);
-    const [unsort, setUnSort] = useState([]);
-    const[branch, setBranch]  = useState([]);
-    const [generate, setGenerate] = useState(false);
-    const [toggleStep, setStep] = useState(true);
-
-
-    function startSort()
-    {
-        
-        let nums=[]
-   
-
-        for(let i=0;i<20;i++)
-        {
-            nums.push(Math.floor(Math.random()*50)+1);
-        }
-
-        setUnSort([...nums]);
-
-        let info = mergeSortingAlgo(nums);
-        setBranch(info[1])
-
-        setSort(info[0]);
-       
+    for (let i = 0; i < 20; i++) {
+      nums.push(Math.floor(Math.random() * 50) + 1);
     }
 
+    setUnSort([...nums]);
 
-    function  resetGenerate()
-    {
-        setGenerate(false);
-        setSort([]);
-        setUnSort([]);
-        setStep(true);
-        props.goToNext(5);
-    }
+    let info = mergeSortingAlgo(nums);
+    setBranch(info[1]);
 
+    setSort(info[0]);
+  }
 
-    function intitiate()
-    {
-        startSort();
-        setGenerate(true);
-        setStep(false);
+  function resetGenerate() {
+    setGenerate(false);
+    setSort([]);
+    setUnSort([]);
+    setStep(true);
+    props.goToNext(5);
+  }
 
-    }
+  function intitiate() {
+    startSort();
+    setGenerate(true);
+    setStep(false);
+  }
 
-    return(
-        <div className="level-container">
+  return (
+    <div className="level-container">
+      <div className="header">
+        <div>Level 4</div>
+        <button
+          onClick={() => {
+            intitiate();
+          }}
+          disabled={generate}
+        >
+          Generate Numbers
+        </button>
+      </div>
 
-            <div className="header">
-                <div >Level 4</div>
-                <button onClick={()=>{intitiate()}} disabled={generate}>Generate  Numbers</button>
-            </div>
-
-      
-                
-            <div className = "array-container">
-
-                <div className="array-layout">
-                   
-                   <div className='array-nums'>
-                    {unsort.map((element, index)=>{
-                        return(
-                            <div key={index} style={{marginLeft: '.5rem' ,marginRight: '.5rem' }}  > {element} </div>
-                        )
-                    })}
-                    </div>
-                 
-
+      <div className="array-container">
+        <div className="array-layout">
+          <div className="array-nums">
+            {unsort.map((element, index) => {
+              return (
+                <div
+                  key={index}
+                  style={{ marginLeft: ".5rem", marginRight: ".5rem" }}
+                >
+                  {" "}
+                  {element}{" "}
                 </div>
-
-
-                <div className = "array-layout">
-                    <div className='array-nums'>
-                    {sort.map((element, index)=>{
-                        return(
-                            <div key={index} style={{marginLeft: '.5rem' ,marginRight: '.5rem' }}  > {element} </div>
-                        )
-                    })}
-                    </div>
-
-                </div>
-      
-            </div>
-             <Steps4 contents={branch} toggle={toggleStep} resetGen={resetGenerate}></Steps4> 
+              );
+            })}
+          </div>
         </div>
-    )
-}
+
+        <div className="array-layout">
+          <div className="array-nums">
+            {sort.map((element, index) => {
+              return (
+                <div
+                  key={index}
+                  style={{ marginLeft: ".5rem", marginRight: ".5rem" }}
+                >
+                  {" "}
+                  {element}{" "}
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+      <Steps4
+        contents={branch}
+        toggle={toggleStep}
+        resetGen={resetGenerate}
+      ></Steps4>
+    </div>
+  );
+};
 
 export default Level4;
