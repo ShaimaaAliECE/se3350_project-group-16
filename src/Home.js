d5da1bb77ddfaae282dd51f733b88d220accaa60
desposto <desposto@uwo.ca>
2022-03-23 20:02:49 -0400

    david changes

--- src/Home.js
+++ src/Home.js
@@ -1,90 +1,68 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import './Home.css';
-import Level1 from './Level1/Level1';
-import Level2 from './Level2/Level2';
-import Level3 from './Level3/Level3';
-import Level4 from './Level4/Level4';
-import Level5 from './Level5/Level5';
+import "./Home.css";
+import Level1 from "./Level1/Level1";
+import Level2 from "./Level2/Level2";
+import Level3 from "./Level3/Level3";
+import Level4 from "./Level4/Level4";
+import Level5 from "./Level5/Level5";
 
+const Home = () => {
+  const [levelOne, setOne] = useState(true);
+  const [levelTwo, setTwo] = useState(false);
+  const [levelThree, setThree] = useState(false);
+  const [levelFour, setFour] = useState(false);
+  const [levelFive, setFive] = useState(false);
 
+  function nextLevel(level) {
+    switch (level) {
+      case 2:
+        setOne(false);
+        setTwo(true);
+        setThree(false);
+        setFour(false);
+        setFive(false);
+        break;
+      case 3:
+        setOne(false);
+        setTwo(false);
+        setThree(true);
+        setFour(false);
+        setFive(false);
+        break;
+      case 4:
+        setOne(false);
+        setTwo(false);
+        setThree(false);
+        setFour(true);
+        setFive(false);
+        break;
+      case 5:
+        setOne(false);
+        setTwo(false);
+        setThree(false);
+        setFour(false);
+        setFive(true);
 
-const Home = ()=>
-{
+        break;
 
-    const [levelOne, setOne] = useState(true);
-    const [levelTwo, setTwo] = useState(false);
-    const [levelThree, setThree] = useState(false);
-    const [levelFour, setFour] = useState(false);
-    const [levelFive, setFive] = useState(false);
-
-
-
-    function nextLevel(level)
-    {
-        
-        switch(level)
-        {
-            case 2:
-                setOne(false)
-                setTwo(true)
-                setThree(false)
-                setFour(false);
-                setFive(false);
-                break;
-            case 3:
-                setOne(false)
-                setTwo(false)
-                setThree(true)
-                setFour(false);
-                setFive(false);
-                break;
-            case 4:
-                setOne(false)
-                setTwo(false)
-                setThree(false)
-                setFour(true);
-                setFive(false);
-                break;
-            case 5:
-                setOne(false)
-                setTwo(false)
-                setThree(false)
-                setFour(false);
-                setFive(true);
-             
-                break;
-            
-            default:
-                console.log('error  in level  selection.')
-
-        }
-
+      default:
+        console.log("error  in level  selection.");
     }
+  }
 
-
-
-
-
-    return(
-
-        <div className="sorting-tutor">
-           <div className="title">Welcome To The Sorting Tutor</div>
-           <div name="body">
-               
-                   
-                         {/* {levelOne&&<Level1 goToNext={nextLevel}/>} 
+  return (
+    <div className="sorting-tutor">
+      <div className="title">Welcome To The Sorting Tutor</div>
+      <div name="body">
+        {/* {levelOne&&<Level1 goToNext={nextLevel}/>} 
                          {levelTwo&&<Level2  goToNext={nextLevel}/>}
                          {levelThree&&<Level3 goToNext={nextLevel}/>} */}
-                         {/* {true&&<Level4 goToNext={nextLevel}/>} */}
-                         {true&&<Level5 goToNext={nextLevel}/>}
-                         
-                     
-           </div>
-           
-        </div>
-
-    );
-}
+        {/* {true&&<Level4 goToNext={nextLevel}/>} */}
+        {true && <Level5 goToNext={nextLevel} />}
+      </div>
+    </div>
+  );
+};
 
 export default Home;
--- src/Level5/Level5.js
+++ src/Level5/Level5.js
@@ -1,99 +1,105 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import {mergeSortingAlgo} from  '../MergeSort';
-import './Level5.css';
-import  Steps4 from './Steps5';
+import { mergeSortingAlgo } from "../MergeSort";
+import "./Level5.css";
+import Steps5 from "./Steps5";
 
-const Level5 = (props)=>{
+const Level5 = (props) => {
+  const [sort, setSort] = useState([]);
+  const [unsort, setUnSort] = useState([]);
+  const [branch, setBranch] = useState([]);
+  const [generate, setGenerate] = useState(false);
+  const [toggleStep, setStep] = useState(true);
 
+  function startSort() {
+    let nums = [];
 
-    const[sort, setSort]=useState([]);
-    const [unsort, setUnSort] = useState([]);
-    const[branch, setBranch]  = useState([]);
-    const [generate, setGenerate] = useState(false);
-    const [toggleStep, setStep] = useState(true);
-
-
-    function startSort()
-    {
-        
-        let nums=[]
-   
-
-        for(let i=0;i<50;i++)
-        {
-            nums.push(Math.floor(Math.random()*100)+1);
-        }
-
-        setUnSort([...nums]);
-
-        let info = mergeSortingAlgo(nums);
-        setBranch(info[1])
-
-        setSort(info[0]);
-       
+    for (let i = 0; i < 50; i++) {
+      nums.push(Math.floor(Math.random() * 100) + 1);
     }
 
+    setUnSort([...nums]);
 
-    function  resetGenerate()
-    {
-        setGenerate(false);
-        setSort([]);
-        setUnSort([]);
-        setStep(true);
-        props.goToNext(5);
-    }
+    let info = mergeSortingAlgo(nums);
+    setBranch(info[1]);
 
+    setSort(info[0]);
+  }
 
-    function intitiate()
-    {
-        startSort();
-        setGenerate(true);
-        setStep(false);
+  function resetGenerate() {
+    setGenerate(false);
+    setSort([]);
+    setUnSort([]);
+    setStep(true);
+    props.goToNext(5);
+  }
 
-    }
+  function levelSelect(select){
+      props.goToNext(select)
+      console.log("hello")
+  }
 
-    return(
-        <div className="level-container">
+  function intitiate() {
+    startSort();
+    setGenerate(true);
+    setStep(false);
+  }
 
-            <div className="header">
-                <div >Level 5</div>
-                <button onClick={()=>{intitiate()}} disabled={generate}>Generate  Numbers</button>
-            </div>
+  return (
+    <div className="level-container">
+      <div className="header">
+        <div>Level 5</div>
+        <button
+          onClick={() => {
+            intitiate();
+          }}
+          disabled={generate}
+        >
+          Generate Numbers
+        </button>
+      </div>
 
-      
-                
-            <div className = "array-container">
-
-                <div className="array-layout">
-                   
-                   <div className='array-nums'>
-                    {unsort.map((element, index)=>{
-                        return(
-                            <div key={index} style={{marginLeft: '.25rem' ,marginRight: '.25rem' }}  > {element} </div>
-                        )
-                    })}
-                    </div>
-                 
-
+      <div className="array-container">
+        <div className="array-layout">
+          <div className="array-nums">
+            {unsort.map((element, index) => {
+              return (
+                <div
+                  key={index}
+                  style={{ marginLeft: ".25rem", marginRight: ".25rem" }}
+                >
+                  {" "}
+                  {element}{" "}
                 </div>
-
-
-                <div className = "array-layout">
-                    <div className='array-nums'>
-                    {sort.map((element, index)=>{
-                        return(
-                            <div key={index} style={{marginLeft: '.25rem' ,marginRight: '.25rem' }}  > {element} </div>
-                        )
-                    })}
-                    </div>
-
-                </div>
-      
-            </div>
-             <Steps4 contents={branch} toggle={toggleStep} resetGen={resetGenerate}></Steps4> 
+              );
+            })}
+          </div>
         </div>
-    )
-}
+
+        <div className="array-layout">
+          <div className="array-nums">
+            {sort.map((element, index) => {
+              return (
+                <div
+                  key={index}
+                  style={{ marginLeft: ".25rem", marginRight: ".25rem" }}
+                >
+                  {" "}
+                  {element}{" "}
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+      <Steps5
+        contents={branch}
+        toggle={toggleStep}
+        resetGen={resetGenerate}
+        lvlSelect={levelSelect}
+      ></Steps5>
+    </div>
+  );
+};
 
 export default Level5;
